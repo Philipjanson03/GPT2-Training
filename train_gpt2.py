4c7858f315af5c58e4b004b41bf8cb790bc49d27
Philipjanson03 <pouyan.ghodrati@gmail.com>
2026-04-30 12:29:05 +0330

    Implement custom weight initialization logic
    - Add _init_weights function to handle standard model initialization
    - Hook up initialization on startup via self.apply(self._init_weights)
    - Add NANO_SCALE_INIT flag to properly scale down residual projections

--- train_gpt2.py
+++ train_gpt2.py
@@ -39,6 +39,7 @@ class CasualSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANO_SCALE_INIT = 1# flag
         # regularization
         self.n_head = config.n_head
         self.n_embd = config.n_embd
@@ -72,6 +73,7 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate='tanh')
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.NANO_SCALE_INIT = 1# flag
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -107,6 +109,22 @@ class GPT(nn.Module):
 
         # weight sharing scheme
         self.transformer.wte.weight = self.lm_head.weight
+
+        # initilize params
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module , 'NANO_SCALE_INIT'):
+                std *= (2 * self.config.n_layer) ** -0.5 # scaling down the activations to control the growth in residual path
+            torch.nn.init.normal_(module.weight, mean = 0.0 , std = std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean = 0, std = 0.02)
+
+
 
     def forward(self, idx, targets = None):
         B,T = idx.size()
@@ -211,6 +229,10 @@ device = "cpu"
 # get a data batch
 if torch.cuda.is_available():
     device = "cuda"
+
+torch.manual_seed(1337)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1337)
 
 train_loader = DataLoaderLite(4,32)
 
